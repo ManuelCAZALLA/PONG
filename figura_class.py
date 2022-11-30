b2c01dcd10fa204b5769e8310cabe11c8ad50bc7
ManuelCAZALLA <manuelcm23@hotmail.com>
2022-11-30 20:13:53 +0100

    ultimos cambios Pang

--- figura_class.py
+++ figura_class.py
@@ -8,6 +8,10 @@ class Pelota:
         self.color = color
         self.vx = vx
         self.vy = vy
+        self.contadorDerecha = 0
+        self.contadorIzquierda = 0
+        self.font = pg.font.Font(None, 40)
+
 
     def dibujar(self,pantalla):
         pg.draw.circle(pantalla,self.color,(self.pos_x,self.pos_y),self.radio)
@@ -16,22 +20,61 @@ class Pelota:
         self.pos_x += self.vx
         self.pos_y += self.vy
 
+        print("posicion x:",self.pos_x+self.radio)
+        print("posicion y:",self.pos_y+self.radio)
+
         if self.pos_y >= y_max-self.radio or self.pos_y < 0+self.radio:
             self.vy *= -1 
         #objetivo que la pelota desaparezca en los limites
         # y vuelva a aparecer rebotando hacia el lado contrario
         #desde donde vino
-        if self.pos_x >= x_max+self.radio*10 or self.pos_x < 0-self.radio*10:
-            #contar el gol
-            self.vx *= -1 
-            self.vy *= -1 
-
-        """
-        if self.pos_x >= x_max-self.radio or self.pos_x < 0+self.radio:
+        #contar el gol nuevo desafio
+        if self.pos_x >= x_max+self.radio*10:#limite derecho
+            self.contadorIzquierda += 1 
             self.vx *= -1
-        if self.pos_y >= y_max-self.radio or self.pos_y < 0+self.radio:
-            self.vy *= -1    
-        """
+            self.vy *= -1
+             
+
+        if self.pos_x < 0-self.radio*10:#limite izquierdo
+            self.contadorDerecha +=1
+            self.vx *= -1
+            self.vy *= -1
+    
+    def marcador(self,pantalla_principal):
+        marcadorIzquierda = self.font.render(str( self.contadorDerecha),0, (255,255,0))
+        marcadorDerecha = self.font.render( str(self.contadorIzquierda),0, (255,255,0))
+        pantalla_principal.blit(marcadorDerecha, (200, 50))
+        pantalla_principal.blit(marcadorIzquierda, (600, 50 ))
+
+    def posicionX(self):
+        return self.pos_x+self.radio
+
+    def posicionY(self):
+        return self.pos_y+self.radio 
+
+    def izquierda(self):
+        if self.pos_x < 400:
+            return True
+        return False
+
+    def derecha(self):
+        if self.pos_x > 400:
+            return True
+        return False
+
+    def arriba(self):
+        if self.pos_y < 300:
+            return True
+        return False
+
+    def abajo(self):
+        if self.pos_y > 300:
+            return True
+        return False             
+    
+
+            
+
 
 
 
@@ -50,10 +93,9 @@ class Raqueta:
         
 
     def mover(self,tecla_arriba,tecla_abajo,y_max=600,y_min=0):
-        estado_teclas = pg.key.get_pressed()
+        estado_teclas = pg.key.get_pressed() # con esto le podemos decir la tecla del teclado que qeremos utilizar
        
-        if estado_teclas[tecla_arriba] == True and self.pos_y > (y_min+self.h//2):
+        if estado_teclas[tecla_arriba] == True and self.pos_y > (y_min+self.h//2): # lo divido para que no se salga de la pantalla
             self.pos_y -= 1
-        if estado_teclas[tecla_abajo] == True and self.pos_y < (y_max-self.h//2) :
+        if estado_teclas[tecla_abajo] == True and self.pos_y < (y_max-self.h//2) : # divido la h con // para que me de un numero entero
             self.pos_y += 1     
-
--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from figura_class import Pelota,Raqueta
-from figura_class import Pelota,Raqueta
 import pygame as pg
 
 pg.init()
@@ -10,35 +9,37 @@ pg.display.set_caption("Pong")
 #definir la tasa de refresco de nuestro bucle de fotogramas fps= fotograma por segundo
 cronometro = pg.time.Clock()
 
-
-pelota = Pelota(400,300)
-raqueta1 = Raqueta(10,300)
+#objetos pelota y raqueta
+pelota = Pelota(400,300) # no le paso los demas valores por que los coge por defecto
+raqueta1 = Raqueta(10,300) # al estar definidos en la clase los demas los coge por defecto
 raqueta2 = Raqueta(790,300)
 
+#asignando velocidad
 raqueta1.vy=5
+pelota.vx=4
 
 
 game_over = False
-
 while not game_over:
-    #imprimir los milisegundos que tarda cada fotograma actualmente
-    vt = cronometro.tick(300)#variable para controlar la velocidad entre fotogramas
+    
+    vt = cronometro.tick(60)#variable para controlar la velocidad entre fotogramas
     #print(vt)
 
     for evento in pg.event.get():
         if evento.type == pg.QUIT:
             game_over = True
 
-    raqueta1.mover(pg.K_w,pg.K_s)
-    raqueta2.mover(pg.K_UP,pg.K_DOWN)
+    raqueta1.mover(pg.K_w,pg.K_s)#mover raqueta1 izquierda
+    raqueta2.mover(pg.K_UP,pg.K_DOWN)#mover raqueta2 derecha
+    pelota.mover()#mover pelota
 
-    pelota.mover()
-
-    pantalla_principal.fill((0,128,94))#pintado de pantalla
+    pantalla_principal.fill((72,156,75))#pintado de pantalla
+ 
+    pelota.marcador(pantalla_principal)#pintado de marcador
     pg.draw.line(pantalla_principal, (255,255,255), (400,0), (400,600), width=2)#pintando linea blanca
+    pelota.dibujar(pantalla_principal)#pintado de pelota
+    raqueta1.dibujar(pantalla_principal)#pintado de raqueta1
+    raqueta2.dibujar(pantalla_principal)#pintado de raqueta2
+    
 
-
-    pelota.dibujar(pantalla_principal)
-    raqueta1.dibujar(pantalla_principal)
-    raqueta2.dibujar(pantalla_principal)
     pg.display.flip()
